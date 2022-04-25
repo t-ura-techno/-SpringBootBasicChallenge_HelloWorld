b64c46951fa0880fc0e18922f2d98b452e0e81e6
t-ura.tjs <t-ura@techno-jouhou.co.jp>
2022-04-25 11:41:31 +0900

    【2022/04/25】プロジェクト名修正

--- src/main/java/com/example/demo/SpringBootBasicApplication.java
+++ src/main/java/com/example/demo/SpringBootBasicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootBasicChallengeHelloWorldApplication {
+public class SpringBootBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBasicChallengeHelloWorldApplication.class, args);
+		SpringApplication.run(SpringBootBasicApplication.class, args);
 	}
 
 }
